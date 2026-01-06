67310e1345a181b3dccc4758f8d9880ffd375423
Tekl <support@tekl.de>
2026-01-06 16:30:42 +0100

    Update to v2026.01.06

--- Casks/beolingus-deutsch-englisch.rb
+++ Casks/beolingus-deutsch-englisch.rb
@@ -1,6 +1,6 @@
 cask "beolingus-deutsch-englisch" do
-  version "2025.03.24"
-  sha256 "dba62bffe4737d9d0b2ec2dfc5f9db25ad4e8da654f934e555abc6c2173d43fa"
+  version "2026.01.06"
+  sha256 "a85b47a13c49ffe8e57100e6666abdbd110fa0f129b505821e604a8daf9daf3b"
 
   url "https://github.com/Tekl/beolingus-deutsch-englisch/releases/download/v#{version}/Beolingus_Deutsch-Englisch_dictionaryfile.zip",
       verified: "github.com/Tekl/beolingus-deutsch-englisch/"
